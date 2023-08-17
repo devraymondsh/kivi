628abfe0959e44b1d2e70cbf7809f764fca78a57
Mahdi Sharifi <devraymondsh@gmail.com>
2023-08-17 23:08:29 +0330

    Add test run for drivers in CI

--- drivers/js/index.d.ts
+++ drivers/js/index.d.ts
@@ -4,5 +4,5 @@ export class Kivi {
 
     del(key: string): string | null;
     get(key: string): string | null;
-    set(key: string, value: string): void;
+    set(key: string, value: string): string;
 }
